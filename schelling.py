d3f04057826c1b19e5c98b53775d1c69a3eb5502
ipora <possantti@gmail.com>
2022-05-28 20:09:51 -0300

    misc

--- schelling.py
+++ schelling.py
@@ -1,7 +1,51 @@
-"""
-Schelling
+'''
 
-"""
+Schelling's Model of Segregation source code
+
+Copyright (C) 2022 Iporã Brito Possantti
+
+References:
+Schelling, Thomas C. (1971).
+"Dynamic models of segregation". The Journal of Mathematical Sociology.
+Informa UK Limited. 1 (2): 143–186.
+doi:10.1080/0022250x.1971.9989794. ISSN 0022-250X.
+
+************ GNU GENERAL PUBLIC LICENSE ************
+
+https://www.gnu.org/licenses/gpl-3.0.en.html
+
+Permissions:
+ - Commercial use
+ - Distribution
+ - Modification
+ - Patent use
+ - Private use
+
+Conditions:
+ - Disclose source
+ - License and copyright notice
+ - Same license
+ - State changes
+
+Limitations:
+ - Liability
+ - Warranty
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+'''
+
 import pandas as pd
 import numpy as np
 import os
